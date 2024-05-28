f0fb92ba409028f084744ea65e7b0c4e9820b725
ahmmed-binas <binasroxz@gmail.com>
2024-05-28 22:05:07 +0200

    doing close review!

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ from urllib.parse import urljoin
 
 
 movies = {}
-
+review_entry = None
+happiness_var = None
+review_window = None
+main_window = None
+DO_NOT_SHOW_FILE = "do_not_show_review.txt"
 
 
 
 
 def gui():
-    global search_var, ratings_var, year_var, genre_var, frame,count_var
+    global window, search_var, ratings_var, year_var, genre_var, frame,count_var
 
     WIDTH = 1000
     HEIGHT = 500
@@ -49,6 +53,9 @@ def gui():
     tk_img = ImageTk.PhotoImage(img)
 
     window.iconphoto(True, tk_img)
+    window.protocol("WM_DELETE_WINDOW" , on_closing)
+
+
     window.mainloop()
 
 
@@ -353,9 +360,65 @@ def clear_movies():
     for widget in frame.winfo_children():
         widget.destroy()
     movies = {}
+    
+    
+def review_box():
+    global review_entry, happiness_var , review_window
+    review_window = tk.Toplevel(window)
+    review_window.title("Review:)")
+    review_window.geometry("400x300")
+    
+    review_label = tk.Label(review_window , text ="Enter Your Review here!")
+    review_label.pack()
+    
+    review_entry = tk.Entry(review_window, width=50)
+    review_entry.pack()
+    
+    happiness_label = tk.Label(review_window,text="Rate Your happines ")
+    happiness_label.pack()
+    
+    happiness_var = tk.StringVar()
+    
+    happiness_frame = tk.frame(review_window)
+    happiness_frame.pack(pady=10)
+    
+    
+    emoji_font = font.Font(family='Helvetica', size=30)  
+
+    happiness_levels = [
+        ("☹️", "Bad"),
+        ("😊", "Good"),
+        ("🙃", "Great"),
+        ("☠️", "Insane")
+    ]
+    
+    for emoji, text in happiness_levels:
+        button_frame = tk.Frame(happiness_frame)
+        button_frame.pack(side=tk.LEFT, padx=10)
+        radio_btn = tk.Radiobutton(button_frame, text=emoji, variable=happiness_var, value=text, font=emoji_font)
+        radio_btn.pack()
+        label = tk.Label(button_frame, text=text)
+        label.pack()
+        
+    submit_btn = tk.Button(review_window, text="Submit", command=send_review)
+    submit_btn.pack(pady=10)
+
+    do_not_show_btn = tk.Button(review_window, text="Don't show this box again", command=disable_review_prompt)
+    do_not_show_btn.pack(pady=5)
+    
+
+
+def close_all():
+    if review_window:
+        review_window.destroy()
+    if window:
+        window.destroy()
+        
+        
+
+
 
 
 
 if __name__ == "__main__":
     gui()
-
